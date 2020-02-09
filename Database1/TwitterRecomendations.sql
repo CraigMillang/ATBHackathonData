19064daed61b859624cbb3aedb016ac1e5e49859
Craig Millang <craig.millang@gmail.com>
2020-02-09 11:45:29 -0700

    Final final checkin Relations changes for Power BI reports

--- Database1/TwitterRecomendations.sql
+++ Database1/TwitterRecomendations.sql
@@ -3,8 +3,10 @@
 	[UserID] NVARCHAR(250) NOT NULL , 
     [SocialNetwork] NVARCHAR(50) NOT NULL, 
     [Handle] NVARCHAR(100) NOT NULL, 
-    [SocialCompany] NVARCHAR(25) NOT NULL, 
+    [SocialCompany] NCHAR(10) NOT NULL, 
     [NumMentions] INT NULL, 
-    [CompanyID] NCHAR(10) NULL, 
-    PRIMARY KEY ([UserID], [SocialNetwork], [Handle],[SocialCompany])
+    [CompanyID] INT NULL, 
+    PRIMARY KEY ([UserID], [SocialNetwork], [Handle],[SocialCompany]), 
+    CONSTRAINT [FK_TwitterRecomendations_CompanyInof] FOREIGN KEY (CompanyID) REFERENCES CompanyInfo(ID), 
+    CONSTRAINT [FK_TwitterRecomendations_User] FOREIGN KEY (UserID) REFERENCES [User](UserName)
 )
---
+++ Database1/User.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserName] NVARCHAR(250) NOT NULL PRIMARY KEY
+)
